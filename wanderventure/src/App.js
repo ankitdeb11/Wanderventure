db765975930259232ea54b532290119eaaafc164
Ankit Deb <deb.ankit.work@gmail.com>
2024-02-08 22:18:14 +0530

    completed work on navbar, made mobile-responsive as well

--- wanderventure/src/App.js
+++ wanderventure/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <div >
-      <h1>Hello Duniya</h1>
+
+
+    <Navbar />
+
+
+
     </div>
   );
 }
