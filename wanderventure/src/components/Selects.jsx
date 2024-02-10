15aa5759a7715cdefbbe28f86332897729e4891e
Ankit Deb <deb.ankit.work@gmail.com>
2024-02-10 12:21:17 +0530

    update commit 2

--- wanderventure/src/components/Selects.jsx
+++ wanderventure/src/components/Selects.jsx
@@ -12,12 +12,12 @@ const Selects = () => {
   return (
     <div className='max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4'>
 
-        <SelectsCard  bg={BoraBora} text='Bora Bora' />
-        <SelectsCard  bg={BoraBora2} text='Antigua' />
-        <SelectsCard  bg={Maldives} text='Maldives' />
-        <SelectsCard  bg={Maldives2} text='Cozumel' />
-        <SelectsCard  bg={Maldives3} text='Jamaica' />
-        <SelectsCard  bg={KeyWest} text='Key West' />
+        <SelectsCard  bg={BoraBora} text='Lakshadweep' />
+        <SelectsCard  bg={BoraBora2} text='Marine Drive' />
+        <SelectsCard  bg={Maldives} text='Andaman & Nicobar' />
+        <SelectsCard  bg={Maldives2} text='Goa' />
+        <SelectsCard  bg={Maldives3} text='Lakshadweep SM' />
+        <SelectsCard  bg={KeyWest} text='Kerala Visits' />
 
     </div>
   )
